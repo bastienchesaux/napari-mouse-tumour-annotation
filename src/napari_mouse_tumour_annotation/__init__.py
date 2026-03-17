8fc43b414f2f454b60088370acdabfb10eb01562
Bastien Chesaux Dapia <bastien.chesauxdapia@epfl.ch>
2026-03-17 09:46:03 +0100

    fix: move  pre-load c10.dll in utily.py

--- src/napari_mouse_tumour_annotation/__init__.py
+++ src/napari_mouse_tumour_annotation/__init__.py
@@ -3,30 +3,10 @@ try:
 except ImportError:
     __version__ = "unknown"
 
-import os
-import platform
+
 import warnings
 
 from ._sample_data import make_sample_data
-
-if platform.system() == "Windows":
-    import ctypes
-    from importlib.util import find_spec
-
-    try:
-        if (
-            (spec := find_spec("torch"))
-            and spec.origin
-            and os.path.exists(
-                dll_path := os.path.join(
-                    os.path.dirname(spec.origin), "lib", "c10.dll"
-                )
-            )
-        ):
-            ctypes.CDLL(os.path.normpath(dll_path))
-    except Exception as e:
-        print(f"Failed to pre-import c10.dll on Windows target with: {e}")
-
 from ._widget import MouseTumourAnnotationQWidget
 
 __all__ = (
